56d7361e3809fd9f42426f9aa7ad5cdea01852f9
nikhil kumar <nikhilpanothra687@gmail.com>
2025-06-30 16:14:01 +0530

    update

---
+++ backend/src/controllers/chat.controller.js
@@ -0,0 +1,15 @@
+import { generateStreamToken } from "../lib/Stream.js";
+
+
+export async function getStreamToken(req,res){
+
+    try {
+        const token = generateStreamToken(req.user.id);
+
+        res.status(200).json({token});
+    
+    } catch (error) {
+         console.error("Error in getStreamToken controller", error.message)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+} 
