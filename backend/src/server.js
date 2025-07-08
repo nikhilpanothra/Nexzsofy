a3e85ec8341cba7c6dc991fa44c7b9e2509884e2
nikhil kumar <nikhilpanothra687@gmail.com>
2025-07-08 18:42:46 +0530

    updated new

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,12 @@ import userRoutes from './routes/user.route.js';
 import chatRoutes from './routes/chat.route.js';
 import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js";
+import path from "path"
 dotenv.config();
 const app  = express()
+const PORT = process.env.PORT
 
-
+const __dirname = path.resolve();
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
@@ -20,13 +22,15 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/chat",chatRoutes);
 
-connectDB()
-if(process.env.NODE_ENV !=="production"){
-   const PORT = process.env.PORT || 5001
-   app.listen(PORT,()=>{
-       console.log(`Server is running on port ${PORT}...`)
-   });
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+    })
 }
 
-export default app;
-
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}...`)
+    connectDB()
+});
